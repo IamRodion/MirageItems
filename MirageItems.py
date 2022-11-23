90b6a3070087e0727080e1396bf6891b115618ee
IamRodion <pablotapias98@gmail.com>
2022-11-22 23:49:30 -0500

    Add menu, log file, addItem function and more
    style(App): Agrega un menú con banner y múltiples opciones
    feat(App): Implementa función Registrar Item
    feat(App): Implementa archivo log para registrar la actividad del código
    feat(App): Implementa temporalmente la función buscar item

--- MirageItems.py
+++ MirageItems.py
@@ -3,18 +3,27 @@ import os, time
 from sqlite3 import Error
 from tabulate import tabulate
 
+BANNER = """███╗   ███╗██╗   ██╗    ██╗████████╗███████╗███╗   ███╗███████╗
+████╗ ████║╚██╗ ██╔╝    ██║╚══██╔══╝██╔════╝████╗ ████║██╔════╝
+██╔████╔██║ ╚████╔╝     ██║   ██║   █████╗  ██╔████╔██║███████╗
+██║╚██╔╝██║  ╚██╔╝      ██║   ██║   ██╔══╝  ██║╚██╔╝██║╚════██║
+██║ ╚═╝ ██║   ██║       ██║   ██║   ███████╗██║ ╚═╝ ██║███████║
+╚═╝     ╚═╝   ╚═╝       ╚═╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚══════╝"""
 
+INFO = "\n[i] Programa que administra items de Mirage Realms en una base de datos SQLite.\n[i] Creado por Rodion (github.com/IamRodion)\n"
+menu1 = """[1] Registrar Item\n[2] Buscar Item\n[3] Modificar Item\n[4] Borrar Item\n[5] Modo Venta\n[6] Salir\n\n[?] Indique una opción: """
+
+def logWrite(text): # Función que registra texto en el archivo de logs.
+    with open('log.txt', 'a') as log: # Abre el archivo de log.
+        fecha, hora = time.strftime("%d/%m/%Y"), time.strftime("%H:%M:%S") # Obteniendo fecha y hora.
+        log.write(f'[{fecha} {hora}] '+text+'\n') # Escribe el texto en el archivo de log.
 
 def clearScreen(): # Función que limpia la pantalla.
     if os.name == 'posix': # Sí es un OS unix se ejecutará el comando "clear".
         os.system("clear")
     else: # En otros casos (windows), se ejecutará "cls".
         os.system("cls")
-
-def logWrite(text): # Función que registra texto en el archivo de logs.
-    with open('log.txt', 'a') as log: # Abre el archivo de log.
-        fecha, hora = time.strftime("%d/%m/%Y"), time.strftime("%H:%M:%S") # Obteniendo fecha y hora.
-        log.write(f'[{fecha} {hora}] '+text+'\n') # Escribe el texto en el archivo de log.
+    logWrite(f"Se ejecutó la función 'clearScreen()' correctamente.")
 
 def createCursor(database): # Genera un objeto cursor a traves de una base de datos.
     conn = None
@@ -29,56 +38,89 @@ def createCursor(database): # Genera un objeto cursor a traves de una base de da
             cursor = conn.cursor()
             logWrite("Se generó correctamente el cursor.")
             return cursor, conn # Devuelve el objeto cursor y el objeto conn para cerrar la conexión a la base de datos al final de la función.
+    logWrite(f"Se ejecutó la función 'createCursor()' correctamente.")
     
-def insertRow(Name, Type, Level, Armour, PrimaryStat, AmountPrimaryStat, Stamina, SecondaryStat, AmountSecondaryStat, BonusModifier1, BonusModifier2, Enchant): # Función que obtiene los valores de un item como argumentos y envía una solicitud SQL para registrarlo en la base de datos.
+def insertRow(Name, Type, Level, Armour, PrimaryStat, AmountPrimaryStat, Stamina, SecondaryStat, AmountSecondaryStat, BonusModifier1, BonusModifier2, Enchant, Sale): # Función que obtiene los valores de un item como argumentos y envía una solicitud SQL para registrarlo en la base de datos.
     cursor, conn = createCursor("MirageItems.db") # Obtiene el objeto cursor y conn de la base de datos.
-    query = f"INSERT INTO Armaduras VALUES ('{Name}', '{Type}', {Level}, {Armour}, '{PrimaryStat}', {AmountPrimaryStat}, {Stamina}, '{SecondaryStat}', {AmountSecondaryStat}, '{BonusModifier1}', '{BonusModifier2}', '{Enchant}')" # Crea una solicitud SQL con las variables una vez obtenidas.
+    query = f"INSERT INTO Armaduras VALUES ('{Name}', '{Type}', {Level}, {Armour}, '{PrimaryStat}', {AmountPrimaryStat}, {Stamina}, '{SecondaryStat}', {AmountSecondaryStat}, '{BonusModifier1}', '{BonusModifier2}', '{Enchant}', '{Sale}')" # Crea una solicitud SQL con las variables una vez obtenidas.
     cursor.execute(query) # Ejecutar una solicitud con el cursor.
+    input(f"\n[i] Se registró el item '{Name}' correctamente, presione Enter.")
     conn.commit() # Aplicar cambios.
     conn.close() # Cerrar conexión.
+    logWrite(f"Se ejecutó la función 'insertRow()' correctamente.")
 
 def createItem(): # Función que obtiene los datos para crear un item y lo envía a la base de datos.
-    Name = input("Name: ")
-    Type = input("Type: ")
-    Level = input("Level: ")
-    Armour = int(input("Armour: "))
-    PrimaryStat = input("Name of Primary Stat: ")
-    AmountPrimaryStat = int(input("Amount of Primary Stat: "))
-    Stamina = int(input("Amount of Stamina: "))
-    SecondaryStat = input("Name of Secondary Stat: ")
-    AmountSecondaryStat = int(input("Amount of Secondary Stat: "))
-    BonusModifier1 = input("First Bonus Modifier: ")
-    BonusModifier2 = input("Second Bonus Modifier: ")
-    Enchant = input("Enchant: ")
-    insertRow(Name, Type, Level, Armour, PrimaryStat, AmountPrimaryStat, Stamina, SecondaryStat, AmountSecondaryStat, BonusModifier1, BonusModifier2, Enchant) # Registra los datos como un nuevo registro en la base de datos.
+    Name = input("[Name]: ")
+    Type = input("[Type]: ")
+    Level = input("[Level]: ")
+    Armour = int(input("[Armour]: "))
+    PrimaryStat = input("[Name of Primary Stat]: ")
+    AmountPrimaryStat = int(input("[Amount of Primary Stat]: "))
+    Stamina = int(input("[Amount of Stamina]: "))
+    SecondaryStat = input("[Name of Secondary Stat]: ")
+    AmountSecondaryStat = int(input("[Amount of Secondary Stat]: "))
+    BonusModifier1 = input("[First Bonus Modifier]: ")
+    BonusModifier2 = input("[Second Bonus Modifier]: ")
+    Enchant = input("[Enchant]: ")
+    Sale = input("[Sale]: ")
+    insertRow(Name, Type, Level, Armour, PrimaryStat, AmountPrimaryStat, Stamina, SecondaryStat, AmountSecondaryStat, BonusModifier1, BonusModifier2, Enchant, Sale) # Registra los datos como un nuevo registro en la base de datos.
+    logWrite(f"Se ejecutó la función 'createItem()' correctamente.")
 
 def showItem(query): # Muestra los datos resultantes de una query en una tabla.
     cursor, conn = createCursor("MirageItems.db") # Obtiene el objeto cursor y conn de la base de datos.
-    cursor.execute(query)
-    data = cursor.fetchall()
-    conn.commit()
-    conn.close()
-    print(tabulate(data, headers=["Name", "Type", "Level", "Armour", "Primary", "Amount", "Stamina", "Secondary", "Amount", "Bonus 1", "Bonus 2", "Enchant"]))
+    cursor.execute(query) # Se ejecuta la solicitud pasada como argumento.
+    data = cursor.fetchall() # Se vuelcan los datos en una variable.
+    conn.commit() # Aplicar cambios
+    conn.close() # Cerrar conexión
+    print(tabulate(data, headers=["Name", "Type", "Level", "Armour", "Primary", "#", "Stamina", "Secondary", "#", "Bonus", "Bonus", "Enchant", "Sale"])) # Muestra los datos almacenados en la variable de forma ordenada en una tabla.
+    logWrite(f"Se ejecutó la función 'showItem()' correctamente.")
+    input("\n[i] Presione Enter para continuar.")
 
-def main():
-    pass
+def showHeader(): # Función que limpia la pantalla, muestra el banner y la información.
+    clearScreen()
+    print(BANNER)
+    print(INFO)
+    logWrite(f"Se ejecutó la función 'showHeader()' correctamente.")   
+
+def mainMenu(): # Función que muestra el menú principal, y devuelve la opción que el usuario desea ejecutar.
+    showHeader()
+    option = input(menu1)
+    logWrite(f"Se ejecutó la función 'mainMenu()' correctamente.")
+    return option
+
+def main(): # Función principal.
+    while True:
+        option = mainMenu()
+        if option == "1": # Registrar Item.
+            showHeader()
+            createItem()
+        elif option == "2": # Buscar Item.
+            showHeader()
+            showItem("SELECT * FROM 'Armaduras' ORDER BY Level")
+        elif option == "3": # Modificar Item
+            pass
+        elif option == "4": # Borrar Item.
+            pass
+        elif option == "5": # Modo Venta
+            pass
+        elif option == "6": # Salir.
+            break
+        else:
+            print("[!] La opción indicada es incorrecta.")
 
 if __name__ == '__main__':
-    main()
-    showItem("SELECT * FROM 'Armaduras'" )
-
-
-
-
-
+    try:
+        main()
+    except KeyboardInterrupt: # En caso que se detenga la ejecución a través del teclado (Ctrl+C) mostrará un mensaje y cerrará el programa
+        print("\nCerrando programa...")
 
 
 # ------------------- Código Reciclado -------------------
 
-#insertRow('Crystal Mask', 'Light', 80, 3, 'Magic', 27, 9, 'N/A', 0, 'Empowered', '3 HP Regen', 'N/A')
-#insertRow('Indigo Hat', 'Light', 60, 3, 'Magic', 21, 7, 'N/A', 0, 'Critic', 'OmniResist', 'N/A')
-#insertRow('Soldier Helmet', 'Medium', 40, 3, 'Distance', 10, 5, 'Defence', 10, '2% Thorns', '2 HP Regen', 'N/A')
-#insertRow('Arnisium Vest', 'Light', 70, 3, 'Magic', 24, 8, 'N/A', 0, 'Empowered', '2% Potions', 'N/A')
+# insertRow('Crystal Mask', 'Light', 80, 3, 'Magic', 27, 9, 'N/A', 0, 'Empowered', '3 HP Regen', 'N/A', 'TRUE')
+# insertRow('Indigo Hat', 'Light', 60, 3, 'Magic', 21, 7, 'N/A', 0, 'Critic', 'OmniResist', 'N/A', 'FALSE')
+# insertRow('Soldier Helmet', 'Medium', 40, 3, 'Distance', 10, 5, 'Defence', 10, '2% Thorns', '2 HP Regen', 'N/A', 'FALSE')
+# insertRow('Arnisium Vest', 'Light', 70, 3, 'Magic', 24, 8, 'N/A', 0, 'Empowered', '2% Potions', 'N/A', 'TRUE')
 
 # if __name__ == '__main__':
 #     create_connection(r"C:\sqlite\db\pythonsqlite.db")
@@ -118,8 +160,11 @@ if __name__ == '__main__':
 #         AmountSecondaryStat int,
 #         BonusModifier1 varchar(100),
 #         BonusModifier2 varchar(100),
-#         Enchant varchar(100)
+#         Enchant varchar(100),
+#         Sale varchar(100)
 #         )""" # Solicitud SQL.
 #     cursor.execute(query) # Ejecutar una solicitud con el cursor.
 #     conn.commit() # Aplicar cambios.
 #     conn.close() # Cerrar conexión.
+
+# createTable()
